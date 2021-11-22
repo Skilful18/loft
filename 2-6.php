c76b13a1145c70758763583b763b605f7a8acfb2
Skilful18 <Skilful93@mail.ru>
2021-11-22 23:03:42 +0300

    поправил файл 2-6

--- 2-6.php
+++ 2-6.php
@@ -2,18 +2,21 @@
 
 // 1. Создайте файл test.txt средствами PHP. Поместите в него текст - “Hello again!”
 
-$file = fopen('test.txt', "w");
-file_put_contents("test.txt", "Hello again!");
+$file = fopen('test.txt', "a");
+//echo file_put_contents("test.txt", "Hello again!");
 
 // 2. Напишите функцию, которая будет принимать имя файла, открывать файл и выводить содержимое на экран.
 
-function funcOpen($file)
+$fp = fopen('test.txt', 'r');
+
+function open($fp)
 {
-    while(!feof($file)){
-        $str = fgetc($file);
+    while (!feof($fp)){
+        $str = fgets($fp, 1024);
         echo $str;
     }
 }
-fclose($file);
-echo funcOpen($file);
 
+echo open($fp);
+
+fclose($fp);
