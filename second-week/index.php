3cda5efaf89253e6c4acaae13e28b7f18127baea
Skilful18 <Skilful93@mail.ru>
2021-11-28 00:10:47 +0300

    задание 3-1 выполнено

--- second-week/index.php
+++ second-week/index.php
@@ -5,3 +5,41 @@ require 'src/functions.php';
 echo task1();
 echo task2();
 echo task3();
+
+for($i = 0; $i < 50; $i++) {
+
+    $users[] = task1();
+
+}
+
+//  Преобразуйте массив в json и сохраните в файл users.json
+
+$json = json_encode($users);
+file_put_contents('users.json', $json);
+
+// Откройте файл users.json и преобразуйте данные из него обратно ассоциативный массив РНР.
+
+$fp = file_get_contents('users.json');
+$decodedUsers = json_decode($fp, true);
+
+//  Посчитайте количество пользователей с каждым именем в массиве
+//  Посчитайте средний возраст пользователей
+
+$names = [];
+$middle_age = 0;
+
+foreach ($decodedUsers as $user) {
+    if(isset($names[$user['name']])) {
+        $names[$user['name']]++;
+    } else {
+        $names[$user['name']] = 1;
+    }
+    $middle_age += $user['age'] / sizeof($decodedUsers);
+}
+
+print_r($middle_age);
+
+
+
+
+
--- second-week/src/functions.php
+++ second-week/src/functions.php
@@ -2,55 +2,43 @@
 
 
 
-function task1()
+function task1(): array
 {
+    $name = ['Andrey', 'Ivan', 'Masha', 'Kseniya', 'Natasha'];
+
+    return [
+        'name' => $name[array_rand($name)],
+        'age' => mt_rand(18,45)
+    ];
+
 }
 function task2()
 {
+//    $i = 1;
+//    $users[] = '';
+//
+//    while ($i <= 50){
+//
+//        $age = mt_rand(18,45);
+//
+//        $name = ['Andrey', 'Ivan', 'Masha', 'Kseniya', 'Natasha'];
+//        $rand = mt_rand(0, count($name) - 1);
+//        $user = ['id' => $i, 'name' => $name[$rand], 'age' => $age];
+//        $users = ["user_$i" => $user];
+//        $i++;
+//
+//        $json = json_encode($users);
+//
+//        file_put_contents('users.json', $json);
+//        echo "<pre>";
+//        print_r($json);
+//
+//    }
 }
 
 function task3()
 {
 
-    $i = 1;
-    while ($i <= 50):
 
-        $name = ['Andrey', 'Ivan', 'Masha', 'Kseniya', 'Natasha'];
-        $rand = mt_rand(0, count($name) - 1);
-        $age = mt_rand(18,45);
-
-        $user = ['id' => $i, 'name' => $name[$rand], 'age' => $age];
-
-        $users = ["user_$i" => $user];
-        $i++;
-
-        echo "<pre>";
-        echo json_encode($users);
-
-    endwhile;
-
-
-//    foreach ($users as $user) {
-//        $a = array_fill_keys($users, 'id');
-//        $b = array_fill_keys($users, 'name');
-//        $c = array_fill_keys($users, 'age');
-//        $users_full = array_merge_recursive($a, $b, $c);
-//    }
-
-//
-//    $key = 'key1';
-//    $value = 'value1';
-//
-//    $array[$key] = $value;
-//
-//    $key = ['mkey1','mkey2','mkey3','mkey4'];
-//    $value = ['mvalue1', 'mvalue2', 'mvalue3', 'mvalue4'];
-//
-//    for ($i = 0; $i<count($key); $i++) {
-//        $array[$key[$i]] = $value[$i];
-//    }
-
-//    echo "<pre>";
-//    print_r($array);
 
 }
